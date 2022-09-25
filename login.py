3edacbe914e37f5044c09ec6772c9352c9b5ef38
Robby Roda <95758113+CoderSigma@users.noreply.github.com>
2022-09-25 09:45:42 +0800

    Create login.py

---
+++ login.py
@@ -0,0 +1,13 @@
+username = 'robby'
+password = 'roda'
+
+userInput = input("What is your username?\n")
+
+if userInput == username:
+    a=input("Password?\n")   
+    if a == password:()
+        print("Welcome!")
+    else:
+        print("That is the wrong password.")
+else:
+    print("That is the wrong username.")
